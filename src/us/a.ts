dff4594ebef2c620e79ea477785e02340b31dc6f
yyobject <48043998+yyobject@users.noreply.github.com>
2021-07-22 21:31:32 +0800

    New translations zh-CN.ts (us)

---
+++ src/us/a.ts
@@ -0,0 +1,15 @@
+/**
+ * @Owner xuchengjian
+ * @Team M3
+ */
+export default {
+  dam_home_page: "Home",
+  dam_statistics: "Statistics",
+  dam_audit: "Review",
+  dam_material: "clip",
+  dam_none: "None",
+  dam_material_group_details: "Asset Group Details",
+  dam_recommend_material_group: "Feed clip",
+  dam_metadata_effect_time: "Entry into force date",
+  dam_metadata_never_expires_time: "Valid forever"
+};
