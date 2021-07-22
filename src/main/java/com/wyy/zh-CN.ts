5138de986464f99e980852c139eff70e43565258
yyobject <48043998+yyobject@users.noreply.github.com>
2021-07-22 21:36:26 +0800

    Update zh-CN.ts

--- src/main/java/com/wyy/zh-CN.ts
+++ src/main/java/com/wyy/zh-CN.ts
@@ -11,5 +11,7 @@ export default {
   dam_material_group_details: '素材组详情',
   dam_recommend_material_group: '推荐素材',
   dam_metadata_effect_time: '即日生效',
-  dam_metadata_never_expires_time: '永久有效'
+  dam_metadata_never_expires_time: '永久有效',
+  dam_aaa:'我是中国人',
+  dam_vvv:'hello',
 };
