267a933c0779f7722df5ffafb0c06da0f1aa99d2
yyobject <48043998+yyobject@users.noreply.github.com>
2021-07-28 09:37:30 +0800

    New translations zh-CN.ts (English)

---
+++ src/English/a.ts
@@ -0,0 +1,17 @@
+/**
+ * @Owner xuchengjian
+ * @Team M3
+ */
+export default {
+  dam_home_page: '首页',
+  dam_statistics: '统计',
+  dam_audit: '审核',
+  dam_material: '素材',
+  dam_none: '无',
+  dam_material_group_details: '素材组详情',
+  dam_recommend_material_group: '推荐素材',
+  dam_metadata_effect_time: '即日生效',
+  dam_metadata_never_expires_time: '永久有效',
+  dam_aaa: '我是中国人',
+  dam_vvv: 'hello'
+};
