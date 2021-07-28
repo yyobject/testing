f9b25683a78da2920780b91fbe5e77e19f4ad9f5
yyobject <48043998+yyobject@users.noreply.github.com>
2021-07-28 09:54:07 +0800

    New translations zh-CN.ts (Japanese)

--- src/Japanese/a.ts
+++ src/Japanese/a.ts
@@ -3,15 +3,15 @@
  * @Team M3
  */
 export default {
-  dam_home_page: '首页',
-  dam_statistics: '统计',
-  dam_audit: '审核',
-  dam_material: '素材',
-  dam_none: '无',
-  dam_material_group_details: '素材组详情',
-  dam_recommend_material_group: '推荐素材',
-  dam_metadata_effect_time: '即日生效',
-  dam_metadata_never_expires_time: '永久有效',
-  dam_aaa: '我是中国人',
-  dam_vvv: 'hello'
+  dam_home_page: "ホーム",
+  dam_statistics: "統計",
+  dam_audit: "モデレート",
+  dam_material: "アセット",
+  dam_none: "いいえ",
+  dam_material_group_details: "バナーグループの詳細",
+  dam_recommend_material_group: "オススメのバナー",
+  dam_metadata_effect_time: "即日発動",
+  dam_metadata_never_expires_time: "恒久的",
+  dam_aaa: "私は中国人",
+  dam_vvv: "helo"
 };
