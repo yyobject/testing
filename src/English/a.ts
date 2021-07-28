cf82d1ec2b97502039544ff9726b6388affdef75
yyobject <48043998+yyobject@users.noreply.github.com>
2021-07-28 09:54:08 +0800

    New translations zh-CN.ts (English)

--- src/English/a.ts
+++ src/English/a.ts
@@ -3,15 +3,15 @@
  * @Team M3
  */
 export default {
-  dam_home_page: '首页',
-  dam_statistics: '统计',
-  dam_audit: '审核',
-  dam_material: '素材',
-  dam_none: '无',
-  dam_material_group_details: '素材组详情',
-  dam_recommend_material_group: '推荐素材',
-  dam_metadata_effect_time: '即日生效',
-  dam_metadata_never_expires_time: '永久有效',
-  dam_aaa: '我是中国人',
+  dam_home_page: "Home",
+  dam_statistics: "Statistics",
+  dam_audit: "Review",
+  dam_material: "clip",
+  dam_none: "None.",
+  dam_material_group_details: "Asset Group Details",
+  dam_recommend_material_group: "Recommended clips",
+  dam_metadata_effect_time: "Entry into force",
+  dam_metadata_never_expires_time: "Valid forever",
+  dam_aaa: "I am Chinese",
   dam_vvv: 'hello'
 };
